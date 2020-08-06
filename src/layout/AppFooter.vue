bac99a06658d835556841e8bd8a0e286ca3b558e
aboaysel <eng.mohamedmos@gmail.com>
2020-08-06 19:42:04 +0200

    refix

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -71,33 +71,14 @@
                                 </h5>
                                 <div class="text-center">
                                     <p class="font-weight-bold text-md-center text-left">{{$ml.get('dar_sama')}}</p>
-                                    <p class="font-weight-bold text-md-center text-left p-0 m-0">
+                                    <p class="text-md-center text-center p-0 m-0">
                                         {{$ml.get('kuwait')}}</p>
                                 </div>
                             </div>
                             <div class="col-12">
                                 <div class="row direction-inverse-mobile">
                                     <div class="col-7 direction">
-                                        <div class="d-flex justify-content-end">
-                                            <ul class="list-unstyled direction m-2 transition"
-                                                style="margin-top: 0!important;">
-                                                <li class="list-inline-item font-weight-bold direction-inverse "
-                                                    v-if="$helper.getSettings().phone1">
-                                                    <a :href="'tel:'+$helper.getSettings().phone1">
-                                                        {{$helper.getSettings().phone1}}
-                                                    </a>
-                                                </li>
-                                                <li class="list-inline-item font-weight-bold direction-inverse"
-                                                    v-if="$helper.getSettings().phone2">
-                                                    <a :href="'tel:'+$helper.getSettings().phone2">
-                                                        {{$helper.getSettings().phone2}}
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                            <img :src="require('@/assets/images/newImages/phone.png')" width="30px"
-                                                 height="30px" class=""
-                                                 alt="">
-                                        </div>
+                                        
                                         <ul class="list-unstyled transition">
                                             <li class="list-inline-item animIcons"
                                                 v-if="$helper.getSettings().facebook">
@@ -131,6 +112,26 @@
                                                 </a>
                                             </li>
                                         </ul>
+                                        <div class="d-flex justify-content-end">
+                                            <ul class="list-unstyled direction m-2 transition"
+                                                style="margin-top: 0!important;">
+                                                <li class="list-inline-item font-weight-bold direction-inverse "
+                                                    v-if="$helper.getSettings().phone1">
+                                                    <a :href="'tel:'+$helper.getSettings().phone1">
+                                                        {{$helper.getSettings().phone1}}
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item font-weight-bold direction-inverse"
+                                                    v-if="$helper.getSettings().phone2">
+                                                    <a :href="'tel:'+$helper.getSettings().phone2">
+                                                        {{$helper.getSettings().phone2}}
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                            <img :src="require('@/assets/images/newImages/phone.png')" width="30px"
+                                                 height="30px" class=""
+                                                 alt="">
+                                        </div>
                                         <div>
                                             <a :href="'mailto:'+$helper.getSettings().front_email">
                                         <span class="p-1"
